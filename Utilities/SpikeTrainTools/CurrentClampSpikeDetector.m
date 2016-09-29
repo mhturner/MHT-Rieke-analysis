69a3dceb1be5bafeb694fe4bc48eefe1359b316e
mturner <mhturner@uw.edu>
2016-09-29 13:51:57 -0700

    Cleaned up and restructured spike detection

--- Utilities/SpikeTrainTools/CurrentClampSpikeDetector.m
+++ Utilities/SpikeTrainTools/CurrentClampSpikeDetector.m
@@ -1,39 +1,70 @@
-function res=CurrentClampSpikeDetector(DataMatrix,threshold,checkDetectionFlag,searchInterval)
-% res=CurrentClampSpikeDetector(DataMatrix,threshold,checkDetectionFlag,searchInterval)
+function [SpikeTimes, SpikeAmplitudes] = CurrentClampSpikeDetector(DataMatrix,varargin)
+% CurrentClampSpikeDetector detects spikes in current-clamp recordings
+%       Pulls indices for peaks above some threshold
+%       good for spikelets or intracellular spikes
+%       just choose threshold high enough s.t. for each threshold crossing there's
+%       only one peak
+%   [SpikeTimes, SpikeAmplitudes] = CurrentClampSpikeDetector(DataMatrix,varargin)
+%   RETURNS
+%       SpikeTimes: In datapoints. Cell array.  Or array if just one trial;
+%       SpikeAmplitudes: Cell array.
+%   REQUIRED INPUTS
+%       DataMatrix: Each row is a trial
+%   OPTIONAL ARGUMENTS
+%       Threshold: (-20) (mV)
+%       CheckDetection: (false) (logical) Plots some stuff
+%       SampleRate: (1e4) (Hz)
+%       SearchWindow; (1.5e-3) (sec) To look for peak after threshold cross
+%   MHT 9.29.2016 - Ported over from personal version. Origional: 2.16.2014
 
-    %Pulls indices for peaks above some threshold
-    %good for spikelets or intracellular spikes
-    %just choose threshold high enough s.t. for each threshold crossing there's
-    %only one peak
-    %MHT 021614
-    for tt = 1:size(DataMatrix,1)
-        trace = DataMatrix(tt,:);
-        spikesUp=getThresCross(trace,threshold,1);
-        inds = [];
-        for ss = 1:length(spikesUp)
-            searchEnd = min(length(trace),(spikesUp(ss)+searchInterval));
-            searchTrace = trace((spikesUp(ss)+1):searchEnd);
-            newDowns = getThresCross(searchTrace,threshold,-1);
-            if isempty(newDowns)
-                continue
-            end
+ip = inputParser;
+ip.addRequired('DataMatrix',@ismatrix);
+addParameter(ip,'Threshold',-20,@isnumeric);
+addParameter(ip,'CheckDetection',false,@islogical);
+addParameter(ip,'SampleRate',1e4,@isnumeric);
+addParameter(ip,'SearchWindow',1.5E-3,@isnumeric);
+    
+ip.parse(DataMatrix,varargin{:});
+DataMatrix = ip.Results.DataMatrix;
+Threshold = ip.Results.Threshold;
+CheckDetection = ip.Results.CheckDetection;
+SampleRate = ip.Results.SampleRate;
+SearchWindow = ip.Results.SearchWindow * SampleRate; % datapoints
 
-            spikeDown = spikesUp(ss)+newDowns(1);
-            [~, ind] = max(trace((spikesUp(ss)+1):spikeDown));
-            inds = cat(2,inds,spikesUp(ss) + ind);
+nTraces = size(DataMatrix,1);
+SpikeTimes = cell(nTraces,1);
+SpikeAmplitudes = cell(nTraces,1);
+
+if (CheckDetection)
+    figure;
+    figHandle = gcf;
+end
+
+for tt = 1:nTraces
+    currentTrace = DataMatrix(tt,:);
+    spikesUp=getThresCross(currentTrace,Threshold,1);
+    inds = [];
+    for ss = 1:length(spikesUp)
+        searchEnd = min(length(currentTrace),(spikesUp(ss)+SearchWindow));
+        searchTrace = currentTrace((spikesUp(ss)+1):searchEnd);
+        newDowns = getThresCross(searchTrace,Threshold,-1);
+        if isempty(newDowns)
+            continue
         end
-
-        if checkDetectionFlag
-            figure(10); clf;
-            plot(trace,'k')
-            hold on
-            plot(inds,threshold.*ones(1,length(inds)),'rx')
-            pause;
-
-        end
-        
-        res.sp{tt} = inds;
-        res.spikeAmps{tt} = trace(inds);
+        spikeDown = spikesUp(ss)+newDowns(1);
+        [~, ind] = max(currentTrace((spikesUp(ss)+1):spikeDown));
+        inds = cat(2,inds,spikesUp(ss) + ind);
     end
+
+    if (CheckDetection)
+        figure(figHandle)
+        plot(currentTrace,'k')
+        hold on
+        plot(inds,Threshold.*ones(1,length(inds)),'rx')
+        pause; clf;
+    end
+    SpikeTimes{tt} = inds;
+    SpikeAmplitudes{tt} = currentTrace(inds);
+end
 
 end
--- Utilities/SpikeTrainTools/SpikeDetector.m
+++ Utilities/SpikeTrainTools/SpikeDetector.m
@@ -1,175 +1,142 @@
-function results = SpikeDetector(D,checkDetectionFlag,epochNo,specialFlag)
-% results = SpikeDetector(D,checkDetectionFlag,epochNo,specialFlag)
-    % MHT last updated 1/28/15
-    %   Took out some extraneous stuff
-    %   Replaced old high pass filter with Debauchies(4) HP filter
-    %   Now Kmeans clustering on 3 variables: peak height, and
-    %   two rebound depths: one to the left and one to the right of
-    %   peak. Search window = searchInterval
-    %   Input
-    %     -D = Response traces in matrix size [trials, epochLength]
-    %   Optional parameters...
-    %     -checkDetectionFlag == 1 will plot some clustering stuff to check
-    %     detection
-    %     -epochNo = for checkDetection figs
-    %     -specialFlag usually for traces with 2 cells. give target
-    %   spike direction 'upward' or 'downward' and don't use rebounds
-    %   for clustering, just peak amps
-    % Output: S.sp = spike times, .spikeAmps, and .violation_ind = refractory
-    % violations associated with S.sp
+function [SpikeTimes, SpikeAmplitudes, RefractoryViolations] = SpikeDetector(DataMatrix,varargin)
+%SpikeDetector detects spikes in an extracellular / cell attached recording
+%   [SpikeTimes, SpikeAmplitudes, RefractoryViolations] = SpikeDetector(dataMatrix,varargin)
+%   RETURNS
+%       SpikeTimes: In datapoints. Cell array. Or array if just one trial;
+%       SpikeAmplitudes: Cell array.
+%       RefractoryViolations: Indices of SpikeTimes that had refractory
+%           violations. Cell array.
+%   REQUIRED INPUTS
+%       DataMatrix: Each row is a trial
+%   OPTIONAL ARGUMENTS
+%       CheckDetection: (false) (logical) Plots clustering information for
+%           each trace
+%       SampleRate: (1e4) (Hz)
+%       RefractoryPeriod: (1.5e-3) (sec)
+%       SearchWindow; (1.2e-3) (sec) To look for rebounds. Search interval 
+%           is (peak time +/- SearchWindow/2)
+%   
+%   Clusters peak waveforms into 2 clusters using k-means. Based on three
+%   quantities about each spike waveform: amplitude of the peak, amlitude
+%   of a rebound on the right and left.
+%   MHT 9.23.2016 - Ported over from personal version
 
-if nargin < 2
-    checkDetectionFlag = 0;
-    epochNo = 0;
-    specialFlag = [];
-elseif nargin <3
-    epochNo = 0;
-    specialFlag = [];
-elseif nargin<4
-    specialFlag = [];
+ip = inputParser;
+ip.addRequired('DataMatrix',@ismatrix);
+addParameter(ip,'CheckDetection',false,@islogical);
+addParameter(ip,'SampleRate',1e4,@isnumeric);
+addParameter(ip,'RefractoryPeriod',1.5E-3,@isnumeric);
+addParameter(ip,'SearchWindow',1.2E-3,@isnumeric);
+    
+ip.parse(DataMatrix,varargin{:});
+DataMatrix = ip.Results.DataMatrix;
+CheckDetection = ip.Results.CheckDetection;
+SampleRate = ip.Results.SampleRate;
+RefractoryPeriod = ip.Results.RefractoryPeriod * SampleRate; % datapoints
+SearchWindow = ip.Results.SearchWindow * SampleRate; % datapoints
+
+nTraces = size(DataMatrix,1);
+DataMatrix =  DB4Filter(DataMatrix,6);
+
+SpikeTimes = cell(nTraces,1);
+SpikeAmplitudes = cell(nTraces,1);
+RefractoryViolations = cell(nTraces,1);
+
+if (CheckDetection)
+    figure;
+    figHandle = gcf;
 end
-if isempty(checkDetectionFlag)
-    checkDetectionFlag = 0;
-end
-% checkDetectionFlag = 0;
 
-SampleInterval = 1E-4;
-ref_period = 1.5E-3; %s
-searchInterval = 1.2E-3; %s, for rebound measurement, looks in (peak_time +/- searchInterval/2)
-
-results = [];
-
-ref_period_points = round(ref_period./SampleInterval);
-searchInterval_points = round(searchInterval./SampleInterval);
-
-[Ntraces,~] = size(D);
-Dhighpass =  DB4Filter(D,6);
-
-sp = cell(Ntraces,1);
-spikeAmps = cell(Ntraces,1);
-violation_ind = cell(Ntraces,1);
-
-for i=1:Ntraces
-    trace = Dhighpass(i,:);
-    if strcmp(specialFlag,'downward') 
-        %do nothing, target peaks already down
-    elseif strcmp(specialFlag,'upward')
-        trace = -trace; %make target peaks down
-    else
-        if abs(max(trace)) > abs(min(trace)) %flip it over, big peaks down
-            trace = -trace;
-        end
-    end
-    
-    
-    %get peaks
-    [peaks,peak_times] = getPeaks(trace,-1); %-1 for negative peaks
-    peak_times = peak_times(peaks<0); %only negative deflections
-    
-    %basically another filtering step:
-    %remove single sample peaks
-    trace_res_even = trace(2:2:end);
-    trace_res_odd = trace(1:2:end);
-    [~,peak_times_res_even] = getPeaks(trace_res_even,-1);
-    [~,peak_times_res_odd] = getPeaks(trace_res_odd,-1);
-    peak_times_res_even = peak_times_res_even*2;
-    peak_times_res_odd = 2*peak_times_res_odd-1;
-    peak_times = intersect(peak_times,[peak_times_res_even,peak_times_res_odd]);
-    peaks = trace(peak_times);
-
-    %get rebounds on either side
-    r = getRebounds(peak_times,trace,searchInterval_points);
-    peakAmps = abs(peaks);
-    
-    if or(strcmp(specialFlag,'downward'),strcmp(specialFlag,'upward')) %don't use rebounds to cluster
-        spikeData = peakAmps';
-        startMat = [median(peakAmps);...
-            max(peakAmps)];
-    else
-        spikeData = [peakAmps',r.Left', r.Right'];
-        startMat = [median(peakAmps) median(r.Left) median(r.Right);...
-            max(peakAmps) max(r.Left) max(r.Right)];
+for tt=1:nTraces
+    currentTrace = DataMatrix(tt,:);
+    if abs(max(currentTrace)) > abs(min(currentTrace)) % flip it over, big peaks down
+        currentTrace = -currentTrace;
     end
 
-    options = statset('MaxIter',10000);
+    % get peaks
+    [peakAmplitudes, peakTimes] = getPeaks(currentTrace,-1); % -1 for negative peaks
+    peakTimes = peakTimes(peakAmplitudes<0); % only negative deflections
+    peakAmplitudes = abs(peakAmplitudes(peakAmplitudes<0)); % only negative deflections
+    
+    % get rebounds on either side of each peak
+    rebound = getRebounds(peakTimes,currentTrace,SearchWindow);
+    
+    % cluster spikes
+    clusteringData = [peakAmplitudes', rebound.Left', rebound.Right'];
+    startMatrix = [median(peakAmplitudes) median(rebound.Left) median(rebound.Right);...
+        max(peakAmplitudes) max(rebound.Left) max(rebound.Right)];
+    clusteringOptions = statset('MaxIter',10000);
     try %traces with no spikes sometimes throw an "empty cluster" error in kmeans
-        [Ind,centroid_amps] = kmeans(spikeData,2,'start',startMat,'Options',options);
+        [clusterIndex, centroidAmplitudes] = kmeans(clusteringData, 2,...
+            'start',startMatrix,'Options',clusteringOptions);
     catch err
         if strcmp(err.identifier,'stats:kmeans:EmptyCluster')
             %initialize clusters using random sampling instead
-            [Ind,centroid_amps] = kmeans(spikeData,2,'start','sample','Options',options);
+            [clusterIndex, centroidAmplitudes] = kmeans(clusteringData,2,'start','sample','Options',clusteringOptions);
         end
     end
-    [~,m_ind] = max(centroid_amps(:,1)); %find cluster with largest peak amplitude
-    n_ind = find(~([1 2]==m_ind)); %nonspike cluster index
-    spike_ind_log = (Ind==m_ind); %spike_ind_log is logical, length of peaks
+    [~,spikeClusterIndex] = max(centroidAmplitudes(:,1)); %find cluster with largest peak amplitude
+    nonspikeClusterIndex = setdiff([1 2],spikeClusterIndex); %nonspike cluster index
+    spikeIndex_logical = (clusterIndex == spikeClusterIndex); %spike_ind_log is logical, length of peaks
     
-    %get spike times and amps
-    sp{i} = peak_times(spike_ind_log);
-    spikeAmps{i} = peakAmps(spike_ind_log);
-    nonSpikeAmps = peakAmps(~spike_ind_log);
+    % get spike times and amplitudes
+    SpikeTimes{tt} = peakTimes(spikeIndex_logical);
+    SpikeAmplitudes{tt} = peakAmplitudes(spikeIndex_logical);
+    nonspikeAmplitudes = peakAmplitudes(~spikeIndex_logical);
     
     %check for no spikes trace
     %how many st-devs greater is spike peak than noise peak?
-    sigF = (mean(spikeAmps{i}) - mean(nonSpikeAmps)) / std(nonSpikeAmps);
+    sigF = (mean(SpikeAmplitudes{tt}) - mean(nonspikeAmplitudes)) / std(nonspikeAmplitudes);
     
     if sigF < 5; %no spikes
-        sp{i} = [];
-        spikeAmps{i} = []; 
-        violation_ind{i} = [];
-        disp(['Epoch ', num2str(epochNo), ', Trial '  num2str(i) ': no spikes!']);
-        if (checkDetectionFlag)
-           figure(1);
-            subplot(1,2,1)
-            plot3(peakAmps(Ind==m_ind), r.Left(Ind==m_ind), r.Right(Ind==m_ind),'ro')
-            hold on;
-            plot3(peakAmps(Ind==n_ind), r.Left(Ind==n_ind), r.Right(Ind==n_ind),'ko')
-            xlabel('PeakAmp'); ylabel('L rebound'); zlabel('R rebound')
-            view([8 36])
-            subplot(1,2,2)
-            plot(trace,'k'); hold on;
-            title([num2str(epochNo), ': No spikes!'])
-            pause(1); clf;
+        SpikeTimes{tt} = [];
+        SpikeAmplitudes{tt} = []; 
+        RefractoryViolations{tt} = [];
+        disp(['Trial '  num2str(tt) ': no spikes!']);
+        if (CheckDetection)
+            plotClusteringData();
         end
-        
         continue
     end
     
-    %check for violations, for warning
-    violation_ind{i} = find(diff(sp{i})<ref_period_points) + 1;
-    ref_violations = length(violation_ind{i});
-    if ref_violations>0
-        disp(['Epoch ', num2str(epochNo), ', trial '  num2str(i) ': ' num2str(ref_violations) ' refractory violations']);
+    % check for refractory violations
+    RefractoryViolations{tt} = find(diff(SpikeTimes{tt}) < RefractoryPeriod) + 1;
+    ref_violations = length(RefractoryViolations{tt});
+    if ref_violations > 0
+        disp(['Trial '  num2str(tt) ': ' num2str(ref_violations) ' refractory violations']);
     end
 
-    if (checkDetectionFlag)
-        figure(1);
-        subplot(1,2,1)
-        plot3(peakAmps(Ind==m_ind), r.Left(Ind==m_ind), r.Right(Ind==m_ind),'ro')
-        hold on;
-        plot3(peakAmps(Ind==n_ind), r.Left(Ind==n_ind), r.Right(Ind==n_ind),'ko')
-        xlabel('PeakAmp'); ylabel('L rebound'); zlabel('R rebound')
-        view([8 36])
-        title(num2str(epochNo))
-        subplot(1,2,2)
-        plot(trace,'k'); hold on;
-        plot(peak_times(spike_ind_log), trace(peak_times(spike_ind_log)),'rx')
-        plot(sp{i}(violation_ind{i}), trace(sp{i}(violation_ind{i})),'go')
-        title(['SpikeFactor = ', num2str(sigF)])
-        if isempty(violation_ind{i})
-            pause(0.1); clf;
-        else
-            pause; clf;
-        end
-        
+    if (CheckDetection)
+        plotClusteringData()
     end
 end
 
-if length(sp) == 1 %return vector not cell array if only 1 trial
-    sp = sp{1};
-    spikeAmps = spikeAmps{1};    
-    violation_ind = violation_ind{1};
+if length(SpikeTimes) == 1 %return vector not cell array if only 1 trial
+    SpikeTimes = SpikeTimes{1};
+    SpikeAmplitudes = SpikeAmplitudes{1};    
+    RefractoryViolations = RefractoryViolations{1};
 end
-results.sp = sp;
-results.spikeAmps = spikeAmps;
-results.violation_ind = violation_ind;
+
+function plotClusteringData()
+    figure(figHandle)
+    subplot(1,2,1); hold on;
+    plot3(peakAmplitudes(clusterIndex==spikeClusterIndex),...
+        rebound.Left(clusterIndex==spikeClusterIndex),...
+        rebound.Right(clusterIndex==spikeClusterIndex),'ro')
+    plot3(peakAmplitudes(clusterIndex==nonspikeClusterIndex),...
+        rebound.Left(clusterIndex==nonspikeClusterIndex),...
+        rebound.Right(clusterIndex==nonspikeClusterIndex),'ko')
+    xlabel('Peak Amplitude'); ylabel('L rebound'); zlabel('R rebound')
+    view([8 36])
+    subplot(1,2,2)
+    plot(currentTrace,'k'); hold on;
+    plot(SpikeTimes{tt}, ...
+        currentTrace(SpikeTimes{tt}),'rx')
+    plot(SpikeTimes{tt}(RefractoryViolations{tt}), ...
+        currentTrace(SpikeTimes{tt}(RefractoryViolations{tt})),'go')
+    title(['SpikeFactor = ', num2str(sigF)])
+    drawnow;
+    pause(); clf;
+end
+
+end
